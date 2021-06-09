7f960e868a8169404c377443d1140ab6def0fff4
Anas <anas.ergeshov.98@gmail.com>
2021-06-09 22:45:27 +0600

    create new permission IsStaff

--- user/permissions.py
+++ user/permissions.py
@@ -16,3 +16,8 @@ class IsSeller(BasePermission):
     def has_permission(self, request, view):
         return bool(request.user and request.user.is_seller)
 
+
+class IsStaff(BasePermission):
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_staff)
